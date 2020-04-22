0be4b0bcd8003b13d9b5dd1865678a107b351288
Kelly Hancox <hancoxk@mail.gvsu.edu>
2020-04-22 14:13:25 -0400

    media query

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,17 +5,21 @@ function NavBar() {
 
     return <div className="navBar">
   
+  <div>
       <Link to="/">
         <span className="logo">
           Wrapped Unwrapped
         </span>
       </Link>
+      </div>
   
+      <div>
         <span>
           <a href="https://accounts.spotify.com/en/logout">
             <button className="logoutButton">Log out</button>
           </a>
         </span>
+        </div>
   
     </div>
   }
